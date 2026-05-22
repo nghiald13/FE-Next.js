54b9ff411a54d1478dddc1fecdaa6087eeb58954
nghiald13 <nghia.ld13@gmail.com>
2026-05-22 15:20:28 +0700

    Seperate components for SignIn (previously was full 'client')

--- src/app/(guest)/auth/signin/page.tsx
+++ src/app/(guest)/auth/signin/page.tsx
@@ -1,9 +1,42 @@
 import { SignInForm } from "@/components/auth/signin-form";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const SignInPage = () => {
 
   return (
-    <SignInForm />
+    <div className="grid min-h-svh lg:grid-cols-2"> {/** Default grid 1 column with min height for devices < lg */}
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+
+        {/* ==================== Brand Logo ==================== */}
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <Avatar >
+                <AvatarImage src="https://pbs.twimg.com/media/G5IXXkfWoAAhjNQ?format=png&name=360x360" alt="Varka" />
+                <AvatarFallback />
+              </Avatar>
+            </div>
+            Want some drinks?
+          </a>
+        </div>
+
+        {/* ==================== Login Area ==================== */}
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignInForm />
+          </div>
+        </div>
+      </div>
+
+      {/* ==================== Half view background image ==================== */}
+      <div className="relative hidden bg-muted lg:block"> {/* Default hidden for <= lg devices, else display block */}
+        <img
+          src="https://gmhsart.weebly.com/uploads/6/4/9/6/6496393/man-1246233-1920_orig.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div >
   )
 }
 
